1bb8fee790c159a22d4a55b8571284af80c53ce3
tomcoretu <tommycorderoetulle@gmail.com>
2017-03-19 00:47:23 +0800

    Update relays.js

--- js/relays.js
+++ js/relays.js
@@ -5,7 +5,7 @@
   var channel3 = 'relay3';
   var channel4 = 'relay4';
 
-  var pubnub = PUBNUB.init({
+  var pubnub = new PubNub({
       subscribe_key: 'sub-c-e0192522-094f-11e7-b8f1-0619f8945a4f',
       publish_key:   'pub-c-aa7ae9c5-6b57-4325-958c-12db93bc3ad5'
   });
