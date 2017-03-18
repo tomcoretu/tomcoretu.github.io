78096a6512768af145b0c8298ebd21a79f3f2078
tomcoretu <tommycorderoetulle@gmail.com>
2017-03-19 00:47:44 +0800

    Update temperature.js

--- js/temperature.js
+++ js/temperature.js
@@ -2,7 +2,7 @@
 
   var channel6 = 'temperature';
 
-  var pubnub = PUBNUB.init({
+  var pubnub = new PubNub({
       subscribe_key: 'sub-c-e0192522-094f-11e7-b8f1-0619f8945a4f',
       publish_key:   'pub-c-aa7ae9c5-6b57-4325-958c-12db93bc3ad5'
   });
